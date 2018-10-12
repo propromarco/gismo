21d687d2d413de2b863b49ef269f759e94f02a26
marco.schwarz <marco.schwarz@meinestadt.de>
2018-10-12 09:15:41 +0200

    Binary Clock

--- weather-station/src/main/resources/static/js/app.js
+++ weather-station/src/main/resources/static/js/app.js
@@ -59,3 +59,50 @@ setInterval(function () {
         }
     });
 }, 60000);
+function display(number, column)
+{
+    led3 = Math.floor(number / 8);
+    led2 = Math.floor((number % 8) / 4);
+    led1 = Math.floor((number % 4) / 2);
+    led0 = Math.floor(number % 2);
+
+    if (column == 0)
+        end = 2;
+    else if (column % 2 != 0)
+        end = 4;
+    else
+        end = 3;
+
+
+    for (i = 0; i < end; i++)
+    {
+        if (eval ("led"+i)) {
+            // document.getElementById("led" + column + i).checked = true;
+            $('#digit' + column + i).addClass('checked');
+        } else {
+            // document.getElementById("led" + column + i).checked = false;
+            $('#digit' + column + i).removeClass('checked');
+        }
+    }
+}
+
+function clock()
+{
+    var  time = new Date();
+
+    var  hrs = time.getHours();
+    display(Math.floor(hrs/10),0);
+    display(hrs%10,1);
+
+    var  min = time.getMinutes();
+    display(Math.floor(min/10),2);
+    display(min%10,3);
+
+    var sec = time.getSeconds();
+    display(Math.floor(sec/10),4);
+    display(sec%10,5);
+
+    setTimeout("clock()",1000);
+}
+
+clock();
